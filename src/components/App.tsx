d04aec16d4da8c8638a64106acf3ec1f5556d184
hanxiao <http://hanxiao7135.dingtalk.com>
2023-12-01 17:01:23 +0800

    init: project init

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import rough from 'roughjs'
+
+
+class App extends React.Component<any, any> {
+
+
+  componentDidUpdate(prevProps: any, prevState: any) {
+  }
+
+  componentDidMount(): void {
+  }
+
+}
+
+export default App
